5ed768e91674c0eff4bbafd698b58c34bb0f9f3b
Karolsz55 <karolsz55@gmail.com>
2023-02-25 08:00:17 +0000

    Add method for printing the board

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ class Battleship:
     Has methods for printing the board adding ships and guesses
     """
     def __init__(self, board_size=5, num_ships=3, ship_size=3):
-        # Initialize the game parameters
+        # Method for initializing the game parameters
         self.board_size = board_size
         self.num_ships = num_ships
         self.ship_size = ship_size
@@ -20,10 +20,15 @@ class Battleship:
         self.player_name = ""
 
     def place_ships(self):
-        # Randomly place the ships on the board
+        # Method for randomly placing the ships on the board
         while self.ships_placed < self.num_ships:
             ship_row = random.randint(0, self.board_size-1)
             ship_col = random.randint(0, self.board_size-1)
             if self.board[ship_row][ship_col] == 'O':
                 self.board[ship_row][ship_col] = 'S'
                 self.ships_placed += 1
+
+    def print_board(self):
+        # Methpd for printing the game board
+        for row in self.board:
+            print(" ".join(row))
